81fb9349167b11a30472d40d1d00b78e135b3e2b
Ryan Eberhardt <reberhardt7@gmail.com>
2015-01-19 16:05:03 -0600

    Fix navbar "stick logic" and add navbar background
    Fix navbar logic so that it sticks properly now that the navbar isn’t
    bottom:0 (there’s a chevron under it). Also add a background when it’s
    stuck, so you can actually read it when there’s text underneath

--- reberhardt/assets/js/app.js
+++ reberhardt/assets/js/app.js
@@ -1,18 +1,36 @@
 // Sticky navbar
 var navbar = $('.navbar')[0];
+var navbarNormalOffset; // Navbar's y-offset when it's not stuck
+var viewportHeight; // Height of viewport that navbarNormalOffset was originally calculated at
 /**
  * Stick the navbar to the appropriate spot based on the scroll position
  */
 function stickNavbar() {
-    navbarTopPosition = window.innerHeight - navbar.offsetHeight;
-    if (navbarTopPosition - document.body.scrollTop > 0)
-        navbar.className = navbar.className.replace('top-anchor', 'bottom-anchor');
-    else navbar.className = navbar.className.replace('bottom-anchor', 'top-anchor');
+    if (document.body.scrollTop < navbarNormalOffset) {
+        // Reset the navbar
+        navbar.className = navbar.className.replace('sticky', 'normal');
+        $('#pane1 .down-chevron').fadeIn();
+    }
+    else {
+        // Sticky the navbar
+        navbar.className = navbar.className.replace('normal', 'sticky');
+        $('#pane1 .down-chevron').fadeOut();
+    }
 }
 
 $(document).ready(function() {
+    // Save the viewport height (see $(window).resize below) and the navbar's position
+    // when it's not sticky (which we need to see if we should unsticky it when scrolling up)
+    viewportHeight = window.innerHeight;
+    navbarNormalOffset = $(navbar).offset().top;
+    // If the document was loaded scrolled down, we might need to stick the navbar already
     stickNavbar();
 })
 $(window).scroll(function() {
     stickNavbar();
+})
+$(window).resize(function() {
+    // Update the normal position of the navbar (with a smaller viewport, it moves up)
+    navbarNormalOffset += (window.innerHeight - viewportHeight);
+    viewportHeight = window.innerHeight;
 })
