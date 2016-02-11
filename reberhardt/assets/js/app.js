3300e20fa601e65833ad6f6d2a4bd0a433aac7a5
Ryan Eberhardt <reberhardt7@gmail.com>
2016-02-11 00:17:42 -0600

    Improve page transitions
    To simplify layout of non-homepage pages, wrap all of the homepage
    elements in a .home-page-container, which is absolutely positioned and
    goes away when we transition to a non-homepage page.
    
    Making .page divs (inside #content) absolutely positioned isn’t
    something I really like. It depends on JS to resize #content when the
    browser can do that better. Get rid of that, and just keep the divs in
    the flow, making sure to set display:none after fading out.
    
    Clean up a lot of the JS handling page transitions…

--- reberhardt/assets/js/app.js
+++ reberhardt/assets/js/app.js
@@ -26,6 +26,11 @@ $('.center-card .nav a').mouseleave(function() {
     }
 });
 
+var transitionTimers = [];
+
+var PAGE_FADE_TRANSITION_TIME = 250;
+var HOME_PAGE_TRANSITION_TIME = 1000;
+
 function loadPage(nextPage) {
     var oldPage = page;
     page = nextPage;
@@ -35,8 +40,51 @@ function loadPage(nextPage) {
     // Don't do anything if we aren't going anywhere
     if(oldPage == nextPage) return;
 
+    // Stop transitions currently in progress
+    for(var i=0; i<transitionTimers.length; i++) {
+        clearTimeout(transitionTimers[i]);
+    }
+    transitionTimers = [];
+
+    function contentDivFadeIn(div) {
+        // This function is used to fade in #content .page divs. The divs first
+        // need to be displayed (i.e. display:block), and then the .show class
+        // needs to be added so that they fade in.
+
+        div.show();
+
+        if(!onMobile) {
+            // We need to use a timeout because if we add the .page class immediately
+            // after making the div visible, then the browser skips the opacity fade.
+            setTimeout(function() { div.addClass('show'); }, 5);
+        }
+        else {
+            div.addClass('show');
+        }
+    }
+
+    function useFirstLoadAnimation() {
+        // This function should be called when transitioning from the home page so
+        // that the first load animations (content div fades in and pans up) are
+        // used.
+        $('#content').addClass('firstload');
+        // We need to clear the firstload class after the page transition is
+        // complete. Otherwise, if we go back to the home page and then click a
+        // different page, the firstload class won't be newly introduced (since it
+        // was already there) and the new content won't drift up.
+        transitionTimers.push(setTimeout(function() {
+            $('#content').removeClass('firstload');
+        }, HOME_PAGE_TRANSITION_TIME));
+    }
+
     if(oldPage == 'home') {
         // We are transitioning in from the home page
+        $('body').removeClass('on-home').addClass('on-content');
+        $('.content-container').append($('.footer'));
+        $('.content-container').show();
+        transitionTimers.push(setTimeout(function() {
+            $('.home-page-container').hide();
+        }, HOME_PAGE_TRANSITION_TIME));
         $('.center-card').removeClass('return').addClass('transition');
         $('.backgrounds').addClass('small');
         $('.footer').removeClass('dark');
@@ -45,11 +93,19 @@ function loadPage(nextPage) {
         // Hide the homepage text once it has transitioned out (or immediately
         // on mobile, on which there are no transitions enabled)
         if(onMobile) $('.center-container').hide();
-        else setTimeout(function(){ $('.center-container').hide(); }, 750);
+        else transitionTimers.push(setTimeout(function(){
+            $('.center-container').hide();
+        }, HOME_PAGE_TRANSITION_TIME - PAGE_FADE_TRANSITION_TIME));
     }
 
     if(nextPage == 'home') {
         // We are transitioning back to the home page
+        $('body').removeClass('on-content').addClass('on-home');
+        $('.home-page-container').append($('.footer'));
+        $('.home-page-container').show();
+        transitionTimers.push(setTimeout(function() {
+            $('.content-container').hide();
+        }, HOME_PAGE_TRANSITION_TIME));
         $('.center-container').show();
         $('.center-card').removeClass('transition').addClass('return');
         $('.backgrounds').removeClass('small');
@@ -87,13 +143,13 @@ function loadPage(nextPage) {
             // We need to make the old text "flip" out and the new text flip in
             $('.banner .content #'+oldPage+'-action').addClass('exiting');
             $('.banner .content #'+nextPage+'-action').addClass('entering');
-            setTimeout(function(){
+            transitionTimers.push(setTimeout(function(){
                 $('.banner .content #'+oldPage+'-action').removeClass('exiting').css('display', 'none');
                 $('.banner .content #'+nextPage+'-action').css('display', 'inline-block');
-            }, 150);
-            setTimeout(function() {
+            }, 150));
+            transitionTimers.push(setTimeout(function() {
                 $('.banner .content #'+nextPage+'-action').removeClass('entering');
-            }, 200);
+            }, 200));
         }
         else {
             $('.banner .content #'+oldPage+'-action').hide();
@@ -104,17 +160,13 @@ function loadPage(nextPage) {
     // We're going to another page, so we hide the current one
     $('#'+oldPage+'-page').removeClass('show');
     if(!onMobile) {
-        // Remove the page from the flow once it transitions out so that it
-        // doesn't add height to the page (even with visibility: hidden, if a
-        // .page div has a greater height than the currently visible .page div,
-        // the page will be scrollable past the footer)
-        setTimeout(function() { $('#'+oldPage+'-page').hide(); }, 250);
+        // Remove the page from the flow once it transitions out
+        transitionTimers.push(setTimeout(function() { $('#'+oldPage+'-page').hide(); }, PAGE_FADE_TRANSITION_TIME));
     }
     else {
         // We don't have transitions so we can remove the div immediately
         $('#'+oldPage+'-page').hide();
     }
-    $('#content').css('height', 0);
 
     // If we are transitioning to some child page (not the home page), we
     // either need to load the new content or display the appropriate already-
@@ -123,7 +175,8 @@ function loadPage(nextPage) {
         // If we don't have an existing .page div for this page, we need to
         // create it and load the new content
         if(!$('#'+nextPage+'-page').length) {
-            var newPageDiv = $('<div>', {id: nextPage+'-page', class: 'page'});
+            var startTime = new Date();
+            var newPageDiv = $('<div>', {id: nextPage+'-page', class: 'page'}).hide();
             $('#content').append(newPageDiv);
             $('#content .loading-spinner').addClass('show');
             newPageDiv.load('/partials/'+nextPage+'.html', function() {
@@ -141,26 +194,23 @@ function loadPage(nextPage) {
                 });
                 function finishLoad() {
                     $('#content .loading-spinner').removeClass('show');
-                    // Since the .page divs are absolutely positioned, they
-                    // take no space in #content, so #content has a height of
-                    // zero and the sticky footer ends up consequently
-                    // floating in the middle of the page. Set #content to the
-                    // visible .page's height in order to avoid this
-                    $('#content').css('height', newPageDiv.height() + 'px');
-                    // If we're coming in from the home page, add the firstload
-                    // class so that the new content "drifts" up when displayed
-                    if(oldPage == 'home' && !onMobile) {
-                        $('#content').addClass('firstload');
-                        // We need to clear the firstload class after the page
-                        // transition is complete. Otherwise, if we go back to the
-                        // home page and then click a different page, the
-                        // firstload class won't be newly introduced (since it was
-                        // already there) and the new content won't drift up.
-                        setTimeout(function() {
-                            $('#content').removeClass('firstload');
-                        }, 1000);
-                    }
-                    newPageDiv.addClass('show');
+                    // Wait until the previous page fades out (if we're on a
+                    // fast connection and this page loaded before the other
+                    // one finished fading out)
+                    var loadTimeElapsed = Number(new Date() - startTime);
+                    var fadeDelay = loadTimeElapsed >= PAGE_FADE_TRANSITION_TIME
+                        ? 1
+                        : PAGE_FADE_TRANSITION_TIME - loadTimeElapsed;
+                    transitionTimers.push(setTimeout(function() {
+                        // If we're coming in from the home page, add the
+                        // firstload class so that the new content "drifts" up
+                        // when displayed
+                        if(oldPage == 'home' && !onMobile) {
+                            useFirstLoadAnimation();
+                        }
+                        // Make the new page fade in
+                        contentDivFadeIn(newPageDiv);
+                    }, fadeDelay));
                 }
                 if(childElemsCount == 0) finishLoad();
             });
@@ -171,30 +221,18 @@ function loadPage(nextPage) {
             // If we're coming in from the home page, add the firstload class
             // so that the new content "drifts" up and in when displayed
             if(oldPage == 'home' && !onMobile) {
-                $('#content').addClass('firstload');
-                // We need to clear the firstload class after the page
-                // transition is complete. Otherwise, if we go back to the
-                // home page and then click a different page, the firstload
-                // class won't be newly introduced (since it was already
-                // there) and the new content won't drift up.
-                setTimeout(function() {
-                    $('#content').removeClass('firstload');
-                }, 1000);
-            }
-            // Show the "new" content
-            $('#'+nextPage+'-page').show();
-            if(!onMobile) {
-                setTimeout(function() { $('#'+nextPage+'-page').addClass('show'); }, 10);
+                useFirstLoadAnimation();
+                // Show the page immediately (the #content div will have a
+                // transition so we don't need to fade in the .page)
+                $('#'+nextPage+'-page').show().addClass('show');
             }
             else {
-                $('#'+nextPage+'-page').addClass('show');
+                // We want to show the new page after the old page has faded
+                // out
+                transitionTimers.push(setTimeout(function() {
+                    contentDivFadeIn($('#'+nextPage+'-page'));
+                }, PAGE_FADE_TRANSITION_TIME));
             }
-            // Since the .page divs are absolutely positioned, they
-            // take no space in #content, so #content has a height of
-            // zero and the sticky footer ends up consequently
-            // floating in the middle of the page. Set #content to the
-            // visible .page's height in order to avoid this
-            $('#content').css('height', $('#'+nextPage+'-page').height() + 'px');
         }
     }
 }
@@ -205,7 +243,7 @@ $(window).resize(function() {
         // inside of #content. Since they're absolutely positioned and take up
         // no space in the flow, when the page reflows we need to manually
         // adjust #content's height to match its visible child div
-        $('#content').css('height', $('#'+page+'-page').height() + 'px');
+        // $('#content').css('height', $('#'+page+'-page').height() + 'px');
     }
 })
 
@@ -221,74 +259,8 @@ $('.nav-link').click(function() {
 
 $(window).on('popstate', function() {
     loadPage(location.pathname.substring(1) != '' ? location.pathname.substring(1) : 'home');
-})
+});
 
 $(document).ready(function() {
     loadPage(location.pathname.substring(1) != '' ? location.pathname.substring(1) : 'home');
-})
-
-
-// // Sticky navbar
-// var navbar = $('.navbar')[0];
-// var navbarNormalOffset; // Navbar's y-offset when it's not stuck
-// var viewportHeight; // Height of viewport that navbarNormalOffset was originally calculated at
-// /**
-//  * Stick the navbar to the appropriate spot based on the scroll position
-//  */
-// function stickNavbar() {
-//     if (document.body.scrollTop < navbarNormalOffset) {
-//         // Reset the navbar
-//         navbar.className = navbar.className.replace('sticky', 'normal');
-//         $('#pane1 .down-chevron').fadeIn();
-//     }
-//     else {
-//         // Sticky the navbar
-//         navbar.className = navbar.className.replace('normal', 'sticky');
-//         $('#pane1 .down-chevron').fadeOut();
-//     }
-// }
-
-// $(document).ready(function() {
-//     if($(navbar).is(':visible')) { // Don't run this on mobile
-//         // Save the viewport height (see $(window).resize below) and the navbar's position
-//         // when it's not sticky (which we need to see if we should unsticky it when scrolling up)
-//         viewportHeight = window.innerHeight;
-//         navbarNormalOffset = $(navbar).offset().top;
-//         // If the document was loaded scrolled down, we might need to stick the navbar already
-//         stickNavbar();
-//     }
-// })
-// $(window).scroll(function() {
-//     if($(navbar).is(':visible')) stickNavbar();
-// })
-// $(window).resize(function() {
-//     if($(navbar).is(':visible')) {
-//         // Update the normal position of the navbar (with a smaller viewport, it moves up)
-//         navbarNormalOffset += (window.innerHeight - viewportHeight);
-//         viewportHeight = window.innerHeight;
-//     }
-// })
-
-
-// // Smooth scrolling when clicking hashes: http://css-tricks.com/snippets/jquery/smooth-scrolling/
-// $(function() {
-//     $('a[href*=#]:not([href=#])').click(function() {
-//         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//             if (target.length) {
-//                 $('html,body').animate({
-//                     scrollTop: target.offset().top
-//                 }, 500);
-//                 // Set hash in browser URL bar
-//                 if(history && history.pushState) {
-//                     history.pushState({}, "", this.hash);
-//                 }
-//                 else {
-//                     window.location.hash = this.hash;
-//                 }
-//                 return false;
-//             }
-//         }
-//     });
-// });
+});
